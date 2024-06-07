d4cac77f2bfd1d46e6fb6f4394142c32aa17b854
Davide Torielli <toridebraus@gmail.com>
2024-06-07 16:17:40 +0200

    fix

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-packages=['iit-gazebo-worlds-pkg'],
-    package_dir={'': 'src'},
+#packages=['iit-gazebo-worlds-pkg'],
+#    package_dir={'': 'src'},
 )
 
 setup(**setup_args)
